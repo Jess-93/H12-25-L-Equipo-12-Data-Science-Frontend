a51b07a99d81b8246771c096b14cbb41d6525aeb
Jesús Villarreal <bash1@live.com.mx>
2026-01-20 22:16:17 -0800

    FIX: TEST URL2

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://flightontime-production.up.railway.app'; // 'http://localhost:8080/api/v1';
+const API_BASE_URL = 'https://flightontime.up.railway.app'; // 'http://localhost:8080/api/v1';
 
 export interface PredictionRequest {
   aerolinea: string;
